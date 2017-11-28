62f027be3758a76008277fbae8e24ad5858537c7
ArlecchinoShion <arlecchino.shion@gmail.com>
2017-11-29 01:02:18 +0900

    vagrant設定ファイル追加

---
+++ provision_init.sh
@@ -0,0 +1,8 @@
+# local develop env initialize shell command
+
+apt-get update
+
+apt-get -y install docker-ce
+curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
