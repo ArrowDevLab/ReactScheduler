94c0998a7b1d7450e3551f86a2e08c2674a52e07
arrow-gunji <s.gunji@arrow-systems.co.jp>
2017-12-01 21:34:19 +0900

    provisionファイル変更

--- provision_init.sh
+++ provision_init.sh
@@ -1,9 +1,22 @@
-# local develop env initialize shell command
+sudo apt-get -y update
+sudo apt-get dist-upgrade
 
-apt-get install docker-ce
+sudo curl -fsSL https://get.docker.com/ | sh
+sudo usermod -aG docker vagrant
+curl -L https://github.com/docker/compose/releases/download/1.10.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+sudo mv docker-compose /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo service docker start
 
-curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+docker network rm common_link
+docker network create --driver bridge common_link
 
-docker -v
-docker-compose -v
+sudo apt-get install -y sl
+
+sudo mkdir /opt/src
+sudo chown -R vagrant:vagrant /opt/src
+
+sudo apt-get -y install samba
+sudo cp -p /vagrant/conf/smb.conf /etc/samba/smb.conf
+sudo chown root:root /etc/samba/smb.conf
+sudo /etc/init.d/samba restart
